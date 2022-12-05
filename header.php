da568c41e6a19f31ce4adc171c81895f44ddd5c7
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-12-05 02:02:57 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<!-- Olá, Eykimim -->
+<!--///////////////////////////////////////////diretriz/////////////////////////////////////////////////-->
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+     <meta charset="utf-8">
+     <title>Mao amiga</title>
+	 <style rel='stylesheet' type='text/css'></style>
+	<link href='cabecalho.css' rel='stylesheet' type='text/css' />
+	<link rel="icon" type="imagem/png" href="img/favicon-16x16.png">
+	<body style=" background-color : white;">
+</head>
+<!--///////////////////////////////////////////////////////////////////////////////////////-->
+
+<!--/////////////////////////////////////////cabecario//////////////////////////////////////////////-->
+	
+		<header>
+			
+         <div class="header-logo">
+
+			<a href="index.php">
+			<img src="img/logo sem fundo.png" vspace="1" hspace="1" />
+			</a>
+			</div>
+			
+			<div class="header-cadlogin">
+			<a href="cadastro.php" >Cadastro</a> 
+			
+			<a href="login.php" >Login</a>
+			</div>	
+		  
+		
+		<div class="header-preto"></div>    	
+
+        </header>
+     
+</html>
+<!--///////////////////////////////////////////////////////////////////////////////////////-->
+
+<!--///////////////////////////////////////////diretriz/////////////////////////////////////////////////-->
+
+	<link href='style.css' rel='stylesheet' type='text/css' />
+
+<!--///////////////////////////////////////////////////////////////////////////////////////-->
