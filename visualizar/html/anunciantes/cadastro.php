82c204c99b1cc1f25c2bb56125bca61de43eac07
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-12-08 13:00:44 -0300

    cadastro

--- visualizar/html/anunciantes/cadastro.php
+++ visualizar/html/anunciantes/cadastro.php
@@ -1,78 +1,74 @@
 <?php include '../header.php'; ?>
 
+<div class="mb-3">
 <center><h2>Catastre-se</h2></center>
+</div>
+<form name="cadastro" method="post" enctype="multipart/form-data" action="/cotroladormysql/salvarcadastro.php">
+  
+<div class="mb-3">
+    <label for="foto" class="form-label">Foto de perfil:</label>
+    <input type="file" name="foto" required placeholder="Foto" id="foto" class="form-control" >
+  </div>
+  <div class="mb-3" >
+<label for="nome" class="form-label">Nome:</label>
+<input type="text" name="nome" required placeholder="Nome" id="nome" class="form-control" aria-describedby="nome">
+ </div>
 
-	<div id="cadastro">
-    	<form name="cadastro" method="post" enctype="multipart/form-data" action="/cotroladormysql/salvarcadastro.php">
-    		<table id="tab_cadastro">
+ 
 
-            	<tr>    
-                    <td>Foto de perfil:</td>
-                    <td><input type="file" name="foto" required placeholder="Foto de Perfil" id="foto" class="txt" /></td>
-                </tr>
-                <tr>
-                	<td>Nome:</td>
-                    <td><input type="text" name="nome" required placeholder="Nome" id="nome" class="txt" /></td>
-                </tr>
-                <tr>
-                	<td>SobreNome:</td>
-                    <td><input type="text" name="sobrenome" required placeholder="sobrenome" id="nome" class="txt" /></td>
-                </tr>
-                <tr>
-                <tr>
-                    <tr>
-                	<td>Nome:</td>
-                    <td><input type="text" name="nome" required placeholder="Nome" id="nome" class="txt" /></td>
-                </tr>
-                <tr>
-                	<td>Nome exibido:</td>
-                    <td><input type="text" name="usuarios" required placeholder="Nome a ser mostrado" id="nomeexibido" class="txt" /></td>
-                </tr>
-                <tr>    
-                    <td>E-mail:</td>
-                    <td><input type="text" name="email" required placeholder="E-mail" id="email" class="txt" /></td>
-                </tr>
-                
-                <tr>    
-                    <td>biogragia:</td>
-                    <td><input type="text" name="biografia" required placeholder="biografia" id="biografia" class="txt" /></td>
-                </tr>
-                <tr>    
-                    <td>cidade:</td>
-                    <td>
-                    <select name="cidade">
-                    <option value="Manhuacu">Manhuaçu</option>
-                    <option value="Manhumirim">Manhumirim</option>
-                    <option value="Simonesia">Simonesia</option>
-                    </select>
-                </td>
-                </tr>
-                <tr>    
-                    <td>telefone:</td>
-                    <td><input type="text" name="telefone" required placeholder="telefone" id="telefone" class="txt" /></td>
-                </tr>
-                <tr>    
-                     <td>servico:</td>
-                    <td><input type="text" name="servico" required placeholder="servico" id="servico" class="txt" /></td>
-                </tr>
-                
-                <tr>    
-                    <td>Senha:</td>
-                    <td><input type="text" name="senha" required placeholder="Senha" id="senhacd" class="txt" /></td>
-                </tr>
-                
-                <tr>
-                    <td>Confirmar Senha:</td> 
-                    <td> <input type="password" name="senhaconf" required placeholder="Confirmar Senha" id="senhacd" class="txt" /></td>
-                </tr>
-                
-                <tr>    
-                    <td colspan="2"><input type="submit" value="Cadastrar" name="cadastrar" id="botao_cad"></td>
-                    <td colspan="2"><input type="reset" value="Cancelar"></td>
-                </tr>
-                
-            </table>
-        </form>
-    </div>
+<div class="mb-3">
+    <label for="nomeexibido" class="form-label">Nome exibido:</label>
+    <input type="text" name="usuarios" required placeholder="Nome a ser mostrado" id="nomeexibido">
+  </div>
 
+
+  <div class="mb-3">
+    <label for="E-mail" class="form-label">E-mail:</label>
+    <input type="text" name="email" required placeholder="E-mail" id="email">
+  </div>
+      
+      <div class="mb-3">
+    <label for="biogragia" class="form-label">biogragia:</label>
+    <input type="text" name="biografia" required placeholder="biografia" id="biografia"  rows="3" required placeholder="E-mail" id="biografia">
+  </div>
+    
+    <select class="form-select" aria-label="cidade:">
+  <option selected>cidade:</option>
+  <option value="Manhuacu">Manhuacu</option>
+  <option value="Manhuacu">Manhuacu</option>
+  <option value="Simonesia">Simonesia</option>
+</select>
+
+<div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">telefone:</label>
+    <input type="text" name="telefone" required placeholder="telefone" id="telefone"  required placeholder="telefone" id="telefone">
+  </div>
+//////////////////
+<div class="mb-3">
+    <label for="servico" class="form-label">serviço:</label>
+    <input type="text" name="servico" required placeholder="biografia" id="biografia"  rows="3" required placeholder="serviço" id="servico">
+  </div>
+
+
+<div class="mb-3">
+<label for="senha" class="form-label">senha</label>
+<input type="senha" name="senha" id="senha" class="form-control" aria-describedby="senha">
+<div id="senha" class="form-text"> Sua senha deve ter de 8 a 20 caracteres, conter letras e números e não deve conter espaços, caracteres especiais ou emoji. </div>
+</div>
+
+<div class="mb-3">
+<label for="senha" class="form-label">Confirmar Senha</label>
+<input type="password" name="senhaconf" required placeholder="Confirmar Senha" id="senhaconf" class="form-control" aria-describedby="senha">
+</div>
+
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="ternos">
+    <label class="form-check-label" for="ternos">Ainda não tem ternos para concordar</label>
+  </div>
+
+  <button type="submit" value="Cadastrar" name="cadastrar" id="cadastrar class="btn btn-primary btn btn-success">Cadastrar</button>
+  <button type="reset" value="Cancelar" class="btn btn-primary btn btn-danger">Cancelar</button>              
+
+
+</form>
     <?php include '../footer.php'; ?>
