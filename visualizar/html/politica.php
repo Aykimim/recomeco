8f0e0795d17766def2c421933f35a4af9c0bb756
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-12-08 10:47:12 -0300

    sera

--- visualizar/html/politica.php
+++ visualizar/html/politica.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 <div class ="container min-vh-100">
-	<div class="row">
+	<div class="row margin-top: calc(2 * var(--bs-gutter-x));">
         
     </br></br></br>
 <h5>Aviso do site ao (a) visitante:</br></br>
